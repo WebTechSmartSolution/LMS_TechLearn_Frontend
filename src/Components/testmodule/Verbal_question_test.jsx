47bc39c73a1d07a7e07264e3ca21ef850474c543
Awais Qadeer <webtechssolution@gmail.com>
2025-02-08 21:55:19 +0500

    testing

--- src/Components/testmodule/Verbal_question_test.jsx
+++ src/Components/testmodule/Verbal_question_test.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { jsPDF } from 'jspdf'; // Import jsPDF
+import jsPDF from 'jspdf';
+ // Import jsPDF
 import './Quiz.css';
 
 const Quiz = () => {
